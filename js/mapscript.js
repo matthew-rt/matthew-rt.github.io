442464fa8c66965cdbbe08df9f1e4bbd781dffcf
Matthew Ryan Tucker <matthew.ryantucker@bristol.ac.uk>
2023-06-26 11:23:52 +0100

    changed to js

---
+++ js/mapscript.js
@@ -0,0 +1,99 @@
+var map = L.map('map', { zoomControl: false, scrollWheelZoom: false }).setView([51.453695, -2.599140], 12.5);
+
+// var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+//     maxZoom: 20,
+//     attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method that we will use to update the control based on feature properties passed
+info.update = function (props) {
+    this._div.innerHTML = '<h4>Average Rent</h4>' + (props ?
+        '<b>' + props.cmwd11nm + '</b><br />' + '£' + props.average_rent + ' per month'
+        : 'Hover over a Hover');
+};
+
+info.addTo(map);
+
+function getColor(d) {
+    return d > 850 ? '#990000' :
+        d > 800 ? '#d7301f' :
+            d > 750 ? '#ef6548' :
+                d > 700 ? '#fc8d59' :
+                    d > 650 ? '#fdbb84' :
+                        d > 600 ? '#fdd49e' :
+                            '#fef0d9';
+}
+function style(feature) {
+    return {
+        fillColor: getColor(feature.properties.average_rent),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+var geojson;
+
+function highlightFeature(e) {
+    const layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    layer.bringToFront();
+
+    info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+    });
+}
+var geojson = new L.GeoJSON.AJAX("rents.geojson", { style: style, onEachFeature: onEachFeature });
+
+geojson.addTo(map);
+
+var legend = L.control({ position: 'bottomright' });
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [550, 600, 650, 700, 750, 800, 850],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
+
