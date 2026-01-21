54dc620ce6fd52d377b7342eaf091847ed61159a
Matthew Ryan Tucker <matthewryantucker@gmail.com>
2026-01-21 22:46:57 +0000

    deleted old data

--- js/doublegraphupdater.js
+++
@@ -1,7 +0,0 @@
-function doubleupdater() {
-    var slider = document.getElementById("hours");
-    var stipendrate = document.getElementById("stipendrate");
-    graphplotter(hours, stipendrate);
-    stipendadjuster(stipendrate);
-
-};
